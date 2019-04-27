64c5e3ec24d00f06a9178f99d28202d206d3212d
XZProd <r.o.profile@gmail.com>
2019-04-27 12:04:02 +0500

    auth

--- json.php
+++ json.php
@@ -10,74 +10,77 @@ use App\User;
 //todo может роутер оформить
 $action = explode('/', $_SERVER['REQUEST_URI'])[2];
 
-if ($action === 'get-users') {
-    $users = User::getUsers();
-    echo json_encode($users);
-}
-
-if ($action === 'get-groups') {
-    $groups = Auth::getGroupsDropdown();
-    echo json_encode($groups);
-}
-
-if ($action === 'get-roles') {
-    $groups = Auth::getRolesDropdown();
-    echo json_encode($groups);
-}
-
-if ($action === 'get-roles_groups') {
-    $groups = Auth::getRolesGroups();
-    echo json_encode($groups);
-}
-
-if ($action === 'update-user') {
-    //мда
-    $entityBody = json_decode(file_get_contents('php://input'));
-    if ($entityBody) {
-        $err = User::updateUser((array)$entityBody);
-        if (User::updateUser((array)$entityBody)) {
-            echo json_encode(['status' => 'success', 'message' => 'успех']);
-        } else {
-            echo json_encode(['status' => 'error', 'message' => $err]);
-        }
+if(User::can(Auth::getUser(), 'canUseAdminpanel'))
+{
+    if ($action === 'get-users') {
+        $users = User::getUsers();
+        echo json_encode($users);
     }
-}
 
-if ($action === 'delete-user') {
-    //мда
-    $entityBody = json_decode(file_get_contents('php://input'));
-    if ($entityBody) {
-        $id = $entityBody->id;
-        if ($id) {
-            if (User::deleteUser($id)) {
+    if ($action === 'get-groups') {
+        $groups = Auth::getGroupsDropdown();
+        echo json_encode($groups);
+    }
+
+    if ($action === 'get-roles') {
+        $groups = Auth::getRolesDropdown();
+        echo json_encode($groups);
+    }
+
+    if ($action === 'get-roles_groups') {
+        $groups = Auth::getRolesGroups();
+        echo json_encode($groups);
+    }
+
+    if ($action === 'update-user') {
+        //мда
+        $entityBody = json_decode(file_get_contents('php://input'));
+        if ($entityBody) {
+            $err = User::updateUser((array)$entityBody);
+            if (User::updateUser((array)$entityBody)) {
                 echo json_encode(['status' => 'success', 'message' => 'успех']);
             } else {
                 echo json_encode(['status' => 'error', 'message' => $err]);
             }
         }
     }
-}
 
-if ($action === 'create-user') {
-    //мда
-    $entityBody = json_decode(file_get_contents('php://input'));
-    if ($entityBody) {
-        if (User::createUser((array)$entityBody)) {
-            echo json_encode(['status' => 'success', 'message' => 'успех']);
-        } else {
-            echo json_encode(['status' => 'error', 'message' => $err]);
+    if ($action === 'delete-user') {
+        //мда
+        $entityBody = json_decode(file_get_contents('php://input'));
+        if ($entityBody) {
+            $id = $entityBody->id;
+            if ($id) {
+                if (User::deleteUser($id)) {
+                    echo json_encode(['status' => 'success', 'message' => 'успех']);
+                } else {
+                    echo json_encode(['status' => 'error', 'message' => $err]);
+                }
+            }
         }
     }
-}
 
-if ($action === 'update-role') {
-    //мда
-    $entityBody = json_decode(file_get_contents('php://input'));
-    if ($entityBody) {
-        if (Auth::updateRights((array)$entityBody)) {
-            echo json_encode(['status' => 'success', 'message' => 'успех']);
-        } else {
-            echo json_encode(['status' => 'error', 'message' => $err]);
+    if ($action === 'create-user') {
+        //мда
+        $entityBody = json_decode(file_get_contents('php://input'));
+        if ($entityBody) {
+            if (User::createUser((array)$entityBody)) {
+                echo json_encode(['status' => 'success', 'message' => 'успех']);
+            } else {
+                echo json_encode(['status' => 'error', 'message' => $err]);
+            }
+        }
+    }
+
+    if ($action === 'update-role') {
+        //мда
+        $entityBody = json_decode(file_get_contents('php://input'));
+        if ($entityBody) {
+            if (Auth::updateRights((array)$entityBody)) {
+                echo json_encode(['status' => 'success', 'message' => 'успех']);
+            } else {
+                echo json_encode(['status' => 'error', 'message' => $err]);
+            }
         }
     }
 }
